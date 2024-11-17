e03e366c13b5b61a00616aeda7c9991d18356d56
morusudev <morusu.contato@gmail.com>
2024-11-17 18:45:28 -0300

    subindo nova versao do projeto com alteração no css e no icon do facebook

--- src/Componets/Main.js
+++ src/Componets/Main.js
@@ -1,47 +1,51 @@
-import styles from './Main.module.css';
+//import logo from './logo.svg';
+import './Main.css';
 
 function App() {
   return (
-    <div className={styles.all_main}>
-      <main>
+    <div class="all_main">
+        <main>
         <section>
-          <div className={styles.images}>
-            <img src="/home-phones-2x.png" width={'420rem'} alt='Telefone1' />
-          </div>
+            <div class="images">
+                <img src="/home-phones-2x.png" width={'420rem'} alt='Telefone1'/>
+            </div>
 
-          <div className={styles.divAlignBox}>
-            <div className={styles.inputsForm}>
-              <h1>Instagram</h1>
-              <form action="#">
-                <div className={styles.alignInputs}>
-                  <input type="text" placeholder="Telefone, nome de usuário ou email" required />
-                  <input type="password" placeholder="Senha" />
-                  <button className={styles.btnClassEnter} id="btnIdEnter" type="submit">Entrar</button>
+            <div class='divAlignBox'>
+                <div class="inputs-form">
+                    <h1>Instagram</h1>
+                    <form action="#">
+                        <div class="alignInputs">
+                            <input type="text" placeholder="Telefone, nome de usuário ou email" required/>
+                            <input type="password" id="" placeholder="Senha"/>
+                            <button class="btnClassEnter" id="btnIdEnter" type="submit">Entrar</button>
+                        </div>
+                    </form>
+    
+                    <div class="alignOr">
+                        <hr/>
+                        <p>OU</p>
+                        <hr/>
+                    </div>
+    
+                    <div class="alignRememberMe">
+                        <div class="alignIconFacebook">
+                            <img src="/facebook.png" width={'20rem'} alt="Facebook"/>
+                            <a href="##">Entrar com Facebook</a>
+                        </div>
+                        <a href="##">Esqueceu a senha?</a>
+                    </div>
                 </div>
-              </form>
-
-              <div className={styles.alignOr}>
-                <hr />
-                <p>OU</p>
-                <hr />
-              </div>
-
-              <div className={styles.alignRememberMe}>
-                <a href="##">Entrar com Facebook</a>
-                <a href="##">Esqueceu a senha?</a>
-              </div>
+    
+                <div class="divBoxNewAccount">
+                    <p>Não tem uma conta?<span><a href="##"> Cadastre-se</a></span></p>
+                </div>
+    
+                <p>Obtenha o aplicativo</p>
+                <div class='alignAppsBtns'>
+                    <img src="/googleplay.png" width={'120rem'} alt="GooglePlay"/>
+                    <img src="/microsoft.png" width={'100rem'} alt="Microsoft"/>
+                </div>
             </div>
-
-            <div className={styles.divBoxNewAccount}>
-              <p>Não tem uma conta?<span><a href="##"> Cadastre-se</a></span></p>
-            </div>
-
-            <p>Obtenha o aplicativo</p>
-            <div className={styles.alignAppsBtns}>
-              <img src="/googleplay.png" width={'120rem'} alt="GooglePlay" />
-              <img src="/microsoft.png" width={'100rem'} alt="Microsoft" />
-            </div>
-          </div>
         </section>
       </main>
     </div>
